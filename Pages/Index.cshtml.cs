5fad0d26bcb0ed828b41a8ae2ba20b5a5ea70a59
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-07-06 18:40:46 -0600

    | Starting Program |

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,6 +6,16 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    // Ticker Symbol
+    private string m_Ticker;
+
+    // Remaining Cash
+    private double m_Cash;
+
+    // Stock Shares
+    private double m_Stocks;
+
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
@@ -28,6 +38,85 @@ public class IndexModel : PageModel
         }
 
     }
+
+    public string GetCash
+    {
+        get
+        {
+            return m_Cash.ToString();
+        }
+    }
+
+    public string GetStocks
+    {
+        get
+        {
+            return m_Stocks.ToString();
+        }
+    }
+
+    public string GetTicker
+    {
+        get
+        {
+            return m_Ticker;
+        }
+    }
+
+    // Create a function that queries the database for the current price at the current day
+    public string CurrentTickerPrice(string tickerName)
+    {
+        double tickerPrice = 0;
+
+        string sql = "SELECT TICKER PRICE FROM TABLE WHERE DATE ";
+
+        // Run query
+
+        // Assign value
+
+        return tickerPrice.ToString();
+    }
+
+    // Create function that sells a certain amount of stock and adds that money for the price of that day to cash but takes away the shares
+    public string SellStocks(string tickerName, string amountSold)
+    {
+        double tickerPrice = 0;
+
+        string sql = "SELECT TICKER PRICE FROM TABLE WHERE DATE ";
+
+        // Run query
+
+        // Assign value
+
+        // Divide the amount sold by the ticker price, and subtract that from the total shares
+
+        // Add that amount sold to the total cash
+
+        // Should I return something else?
+
+        return tickerPrice.ToString();
+    }
+
+    // Create function that buys a certain amount of stock based on an amount of cash and adds it to shares but takes it from cash
+    public string BuyStocks(string tickerName, string amountBuy)
+    {
+        double tickerPrice = 0;
+
+        string sql = "SELECT TICKER PRICE FROM TABLE WHERE DATE ";
+
+        // Run query
+
+        // Assign value
+
+        // Divide the amount to buy by the ticker price, and add that to the total shares
+
+        // Subtract that amount sold to the total cash
+
+        // Should I return something else?
+
+        return tickerPrice.ToString();
+    }
+
     public void OnGet()
     {
 
